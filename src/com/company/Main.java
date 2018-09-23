93ab8928181d5cc2177f11c29eb99ba9ff5ecb0e
emilyobrien17 <e6obrien@student.bridgew.edu>
2018-09-23 19:44:29 -0400

    inital

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	    int total = 0;
+        Scanner keyboard = new Scanner(System.in);
+        for (int i = 1; i < 6; i++) {
+            System.out.println("What are the shifts in industry " + i + "?");
+            for (int j = 0; j < 12; j++) {
+                total = total + keyboard.nextInt();
+            }
+        }
+        System.out.println("Total industry shift: " + total);
+    }
+
+}
